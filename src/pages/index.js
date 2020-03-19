eb42cd30f44c54882d078d9324eb08669f2b8b25
prasanthsaikanumuri <41641628+prasanthsaikanumuri@users.noreply.github.com>
2020-03-19 21:20:03 +0530

    added zomato api

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,11 +17,12 @@ class IndexPage extends React.Component{
   }
 
   getLatLong(locationName){
+  
     fetch(`https://us1.locationiq.com/v1/search.php?key=93413dd7e03322&q=${locationName}&format=json`,
     {
       method: 'GET',
       headers: {
-        'accept': 'application/json',
+        'accept': 'application/json'
       }
     })
     .then(res => res.json())
@@ -31,20 +32,40 @@ class IndexPage extends React.Component{
           lat: data[0].lat,
           lon: data[0].lon
         }
-      });
+      })
       this.getRestaurantData(data[0].lat, data[0].lon);
     });
   }
 
   getRestaurantData(Lat,Long){
-    console.log(Lat);
-    console.log(Long);
-
+    try{
+      fetch(`https://developers.zomato.com/api/v2.1/search?lat=${Lat}&lon=${Long}&entity_type=landmark&radius=100`,
+    {
+     method: "GET",
+     headers:{
+      'accept': 'application/json',
+      'user-key': "402f8842a1e6605bdc5f03d9d9c202db"
+     } 
+    })
+    .then(res => res.json())
+      .then(data =>{
+        //console.log(data.restaurants)
+        this.setState({
+          restaurants: data.restaurants
+        });
+        
+      })
+      
+  
+    }
+    catch(e){
+      console.log(e);
+    }
   }
 
   handleOnClick(){
     // console.log(this.state.locationName);
-    getLatLong(this.state.locationName);
+    this.getLatLong(this.state.locationName);
   }
 
   render (){
