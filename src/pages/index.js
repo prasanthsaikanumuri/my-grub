ecd421bd5e3d1c67b7d9e785708b76ba83e85251
prasanthsaikanumuri <41641628+prasanthsaikanumuri@users.noreply.github.com>
2020-03-20 12:27:07 +0530

    index.js complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,42 @@ import SEO from "../components/seo"
 import './index.css';
 import fetch from 'node-fetch';
 
+const RestrauntList = ({ restaurants, onClickHandler, favs }) => {
+  return (
+    restaurants ? <div className="restaurant-list">
+      {restaurants.map(({restaurant}) => (
+        <div className="restaurant-container" key={`restaurant${restaurant.id}`}>
+          <span className="user-rating">{restaurant.user_rating.aggregate_rating}</span>
+          <Link to={`/restaurant/?id=${restaurant.id}`}>
+            <img src={restaurant.thumb} />
+          </Link>
+          <p>
+            {restaurant.name}
+            <i 
+              onClick={() => onClickHandler(restaurant.id)} 
+              className={`fa fa-heart ${favs.indexOf(restaurant.id) > -1 ? 'selected' : ''}`}
+            ></i>
+          </p>
+        </div>
+      ))}
+    </div> : null
+  )
+}
 
 class IndexPage extends React.Component{
+
+  constructor() {
+    super();
+
+    this.state = {
+      //restaurants: restaurantsData.restaurants,
+      restaurants: null,
+      locationName: '',
+      favs: []
+    };
+  }
+  
+  
   setLocationNameOnChange(evt){
     this.setState({
       locationName: evt.target.value
@@ -54,9 +88,9 @@ class IndexPage extends React.Component{
           restaurants: data.restaurants
         });
         
-      })
+      });
       
-  
+  //console.log(this.state)
     }
     catch(e){
       console.log(e);
@@ -66,6 +100,25 @@ class IndexPage extends React.Component{
   handleOnClick(){
     // console.log(this.state.locationName);
     this.getLatLong(this.state.locationName);
+  }
+
+  addToFavs(res_id) {
+    const resIndex = this.state.favs.indexOf(res_id);
+    let favs = [];
+
+    if (resIndex > -1) {
+      this.state.favs.splice(resIndex, 1);
+
+      favs = [...this.state.favs];
+
+      console.log(favs)
+    } else {
+      favs = [...this.state.favs, res_id]
+    }
+
+    this.setState({
+      favs: favs
+    });
   }
 
   render (){
@@ -78,13 +131,26 @@ class IndexPage extends React.Component{
       {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div> */}
-      <div className="location-search">
+        <h2>Order from various restaurants near you right from your home!</h2>
+        <h4>To get started, enter your city name/location</h4>
+          <div className="location-search">
             <input placeholder="Enter the location" onChange={(evt) => this.setLocationNameOnChange(evt)}/>
             <button onClick={(evt) => this.handleOnClick(evt)}>Search</button>
           </div>
-      console.log(locationName);
+          {/* {console.log(this.state)} */}
+      { this.state.restaurants
+          ? <RestrauntList onClickHandler={(id) => this.addToFavs(id)} restaurants={this.state.restaurants} favs={this.state.favs} />
+          : <div>
+            <img src="https://images.unsplash.com/photo-1522906456132-bac22adad34e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" />
+          </div>
+        }
+        { this.state.loading && <div className="loading">
+            <img src="https://icon-library.net//images/gif-loading-icon/gif-loading-icon-17.jpg" />
+          </div>
+        }
       
-      <Link to="/page-2/">Go to page 2</Link>
+      {/* <Link to="/page-2/">Go to page 2</Link> */}
+      {/* <RestrauntList onClickHandler={(id) => this.addToFavs(id)} restaurants={this.state.restaurants} favs={this.state.favs} /> */}
     </Layout>
   )
   }
---
+++ src/pages/restaurant.js
@@ -0,0 +1,24 @@
+import React from "react"
+//import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+//import SEO from "../components/seo"
+
+const RestaurantsList = () =>{
+  return(
+  <h1>Hi</h1>
+  )
+}
+
+class Restaurants extends React.Component{
+
+  render(){
+    return(
+      <Layout>
+        <RestaurantsList/>
+    </Layout>
+    )
+  }
+}
+
+export default Restaurants
